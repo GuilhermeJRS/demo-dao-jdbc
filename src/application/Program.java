4498496e7ae62f617bbac2da8e87ed744bae585f
Guilherme Souza <guilhermejrs@gmail.com>
2021-01-29 09:30:12 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,10 @@
 package application;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,6 +12,9 @@ public class Program {
 
 		Department dpto = new Department (1, "Books"); 
 		System.out.println(dpto);
+
+		Seller seller = new Seller (21, "Bob", "bob@gmail.com", new Date(), 1780.00, dpto); 
+		System.out.println(seller);
 		
 	}
 
