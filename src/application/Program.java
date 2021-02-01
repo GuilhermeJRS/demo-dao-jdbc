c6b0216fe1308c874a2884d11621afa43a97213f
Guilherme Souza <guilhermejrs@gmail.com>
2021-02-01 16:49:27 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,9 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,6 +14,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println("===== TEST 1: seller findById =====");
@@ -19,15 +22,20 @@ public class Program {
 		System.out.println(sellerDao.findById(4));	
 		
 		System.out.println("===== TEST 2: seller findByDepartment =====");
-		List <Seller> sellers = sellerDao.findByDepartment(new Department (2, null));
+		List <Seller> sellers = sellerDao.findByDepartment(new Department(2, null));
 		for (Seller seller : sellers) {
 			System.out.println(seller);			
 		}
 		
-		System.out.println("===== TEST 2: seller findAll =====");
+		System.out.println("===== TEST 3: seller findAll =====");
 		sellers = sellerDao.findAll();
 		for (Seller seller : sellers) {
 			System.out.println(seller);			
 		}
+		
+		System.out.println("===== TEST 4: seller insert =====");
+		Seller newSeller = new Seller(null, "Greeeeeg", "greeeeeg@gmail.com", new Date(), 4000.0, new Department(3, null));
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New Id = " + newSeller.getId());
 	}
 }
